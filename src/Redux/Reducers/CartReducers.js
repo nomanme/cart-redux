b7f02955ce8eeec3fceb29d36a7735d699c225aa
nomanme <abdullah.noman083@gmail.com>
2020-09-17 00:39:06 +0600

    added

---
+++ src/Redux/Reducers/CartReducers.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../Action/CartAction';
+
+const initialState = {
+    cart: [],
+    // products: [
+    //     { name: 'Lenovo Laptop', id: 1 },
+    //     { name: 'Afsus Laptop', id: 2 },
+    //     { name: 'Dell Laptop', id: 3 },
+    //     { name: 'HP Laptop', id: 4 },
+    //     { name: 'Toshiba Laptop', id: 5 }
+    // ]
+}
+
+const CartReducers = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+            const newItem = {
+                productId: action.id,
+                name: action.name,
+                cartId: state.cart.length + 1
+            }
+            const newCart = [...state.cart, newItem];
+            return { ...state, cart: newCart };
+
+        case REMOVE_FROM_CART:
+            const cartId = action.cartId;
+            const remainingCart = state.cart.filter(item => item.cartId !== cartId);
+            return { ...state, cart: remainingCart }
+
+        default:
+            return state;
+    };
+}
+
+export default CartReducers;
